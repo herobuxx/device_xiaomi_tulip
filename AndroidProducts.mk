82d2aa87bfb9516393e3a00d317a9cdc968a0955
HeroBuxx <me@herobuxx.xyz>
2020-12-23 20:50:58 +0000

    tulip: ConquerOS Bringup
    Signed-off-by: HeroBuxx <me@herobuxx.xyz>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_tulip.mk
+    $(LOCAL_DIR)/conquer_tulip.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_tulip-user \
-    lineage_tulip-userdebug \
-    lineage_tulip-eng
+    conquer_tulip-user \
+    conquer_tulip-userdebug \
+    conquer_tulip-eng
